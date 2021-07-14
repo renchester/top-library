b3176fea52005be80abbe3275930730bb8a94fd4
Renchester Ramos <renchesterjramos@gmail.com>
2021-07-15 03:37:03 +0800

    Add library management ffunctionality

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // DOM Selectors
 
-const bookCards = document.querySelectorAll('.book-card');
+const bookContainer = document.querySelector('.book-container');
 const btnAddBook = document.querySelector('.btn-add-book');
 
 const formContainer = document.querySelector('.form-container');
@@ -44,11 +44,75 @@ function addBookToLibrary(e) {
     bookHasRead.value
   );
 
+  myLibrary.push(book);
+  displayBooks();
+
   form.reset();
   hideForm();
 }
 
-function displayBooks() {}
+function delBookFromLibrary(e) {
+  const bookToDelete = myLibrary.find(
+    (book) =>
+      `${book.author.split(' ').splice(-1, 1)}-${book.title}` ===
+      e.target.closest('.book-card').dataset.id
+  );
+
+  const indexToDelete = myLibrary.indexOf(bookToDelete);
+
+  myLibrary.splice(indexToDelete, 1);
+
+  displayBooks();
+}
+
+function displayBooks() {
+  bookContainer.innerHTML = '';
+
+  if (!myLibrary.length) return;
+
+  myLibrary.forEach((book) =>
+    bookContainer.insertAdjacentHTML(
+      'afterbegin',
+      ` 
+      <div class="book-card" data-id="${book.author.split(' ').splice(-1, 1)}-${
+        book.title
+      }">
+        <div class="book">
+          <p class="book-title">${book.title}</p>
+          <p class="book-author">${book.author}</p>
+          <div class="book-hover hidden">
+            <div class="book-window">
+              <p class="pages-wrapper">
+                <span class="book-pages">${book.pages}</span> pages read since
+              </p>
+              <p class="book-start">July 30, 2021</p>
+            </div>
+            <button class="btn btn-del-book">
+              <span class="material-icons icons-delete"> close </span>
+            </button>
+          </div>
+        </div>
+        <!-- <div class="book-info">
+          <p class="book-has-read">60%</p>
+        </div> -->
+      </div>`
+    )
+  );
+
+  addHover();
+}
+
+function addHover() {
+  const bookCards = document.querySelectorAll('.book-card');
+  const btnDelBook = document.querySelector('.btn-del-book');
+
+  bookCards.forEach((book) => {
+    book.addEventListener('mouseover', hoverOnBook);
+    book.addEventListener('mouseout', hoverOnBook);
+  });
+
+  btnDelBook.addEventListener('click', delBookFromLibrary);
+}
 
 function hoverOnBook(e) {
   const book = document.querySelector('.book-hover');
@@ -70,11 +134,34 @@ function showForm(e) {
 }
 
 function validateBook(arr) {
-  console.log(arr);
-  const [title, author, pages, readStatus] = arr;
+  const [title, author, pages, hasRead] = arr;
+
+  document
+    .querySelectorAll('.error-message')
+    .forEach((el) => (el.textContent = ''));
 
   if (!title) {
-    const titleError = 1;
+    document.querySelector(
+      '.error-message.error-title'
+    ).textContent = `Title input is invalid.`;
+  }
+
+  if (!author) {
+    document.querySelector(
+      '.error-message.error-author'
+    ).textContent = `Author input is invalid.`;
+  }
+
+  if (!hasRead || hasRead === 'none') {
+    document.querySelector(
+      '.error-message.error-read'
+    ).textContent = `Read status input is invalid.`;
+  }
+
+  if (!title || !author || !hasRead) {
+    return false;
+  } else if (title && author && hasRead) {
+    return true;
   }
 }
 
@@ -86,10 +173,6 @@ const book1 = new Book(
 );
 
 // Event Listeners
-bookCards.forEach((book) => {
-  book.addEventListener('mouseover', hoverOnBook);
-  book.addEventListener('mouseout', hoverOnBook);
-});
 
 btnAddBook.addEventListener('click', showForm);
 
