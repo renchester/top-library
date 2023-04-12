b12681baebcbaab4e138ca549b4f959e5bd2e4d7
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-13 07:45:13 +0800

    Remove libraryController from index initializer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import LibraryController from './controllers/libraryController';
 import AuthController from './controllers/authController';
 
 import './css/style.css';
 
 const init = () => {
-  LibraryController.initializeLibrary();
   AuthController.initializeAuth();
 };
 
